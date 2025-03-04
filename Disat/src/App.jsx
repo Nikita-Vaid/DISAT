2e0744029d321789f5370c15babf7e944d4e122e
RaviRanjan20002 <raviranjankumar130082002@gmail.com>
2025-03-04 16:44:45 +0530

    added

--- Disat/src/App.jsx
+++ Disat/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registration from "./Component/Pages/Registration";
+import Login from "./Component/Pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Registration/>} /> 
+        <Route path="/" element={<Login/>} /> 
+        <Route path="/iacstexam" element={<Registration/>} /> 
       </Routes>
     </BrowserRouter>
   );
