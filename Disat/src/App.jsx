ee65b7e6c354ee3218e8c11ed176ed94c2a919ec
RaviRanjan20002 <raviranjankumar130082002@gmail.com>
2025-03-05 10:13:37 +0530

    updated

--- Disat/src/App.jsx
+++ Disat/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registration from "./Component/Pages/Registration";
 import Login from "./Component/Pages/Login";
+import Layout from "./Component/Section/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} /> 
-        <Route path="/iacstexam" element={<Registration/>} /> 
+      <Route path ="/" element ={<Layout/>}>
+        <Route path="/" element={<Login/>}/>
+        <Route path="iacstexam" element={<Registration/>} /> 
+        </Route>
       </Routes>
     </BrowserRouter>
   );
