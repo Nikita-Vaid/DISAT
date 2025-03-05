32a7a50a07e846bd8066da231b614948ea94d60a
Nikita-Vaid <vaidnikita06@gmail.com>
2025-03-05 14:55:18 +0530

    navbar

--- Disat/src/Component/Pages/Login.jsx
+++ Disat/src/Component/Pages/Login.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import loginpic from "../../assets/login.png";
-import "../../Styles/login.css"
+import "../../Styles/login.css";
+import { FaRegCalendarAlt } from "react-icons/fa";
+
 const Login = () => {
   return (
     <div className="login-container">
-      {/* Left Side - Image */}
+      {/* Left Side - Image Section */}
       <div className="login-left">
         <img src={loginpic} alt="Students" className="login-image" />
+        <div className="image-text">
+          <h1>Aakash</h1>
+          <p>Medical IIT-JEE Foundations</p>
+          <span>A scholarship exam for class 5th to 12th studying and passed students</span>
+        </div>
       </div>
 
       {/* Right Side - Login Form */}
@@ -18,8 +25,12 @@ const Login = () => {
           <input type="text" placeholder="Enter your roll number" required />
 
           <label>Password</label>
-          <input type="date" required />
-          {/* <a href="/" className="forgot-link">Forgot roll number/password?</a> */}
+          <div className="password-field">
+            <input type="date" required />
+            <FaRegCalendarAlt className="calendar-icon" />
+          </div>
+
+          <a href="/" className="forgot-link">Forgot roll number/password?</a>
 
           <button type="submit" className="login-btn">Login</button>
 
@@ -33,4 +44,3 @@ const Login = () => {
 };
 
 export default Login;
-
